66e90fe6c5a5185b33e3eade1265726af5763799
Pranit Deshpande <71553492+pranitdeshpande98@users.noreply.github.com>
2024-08-26 19:33:53 -0400

    deployment related sentry changes

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,14 +5,13 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://984f8d8f9acd7307a70cb038e7cbeb26@o4507584539983872.ingest.us.sentry.io/4507840308117504",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://3d627de24f5d06a1fc39000a06ca9a94@o4506813739368448.ingest.us.sentry.io/4507458386526208",
+  dsn: "https://984f8d8f9acd7307a70cb038e7cbeb26@o4507584539983872.ingest.us.sentry.io/4507840308117504",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
